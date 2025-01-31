a69310168cbccfe942e8a7253075bfd6eca9621f
rmochi <rm_mugen@yahoo.co.jp>
2025-02-01 02:11:07 +0900

    init

---
+++ _privateInfo.sample.ts
@@ -0,0 +1,58 @@
+// 請求内容
+export const billingItems = [
+	{
+		name: "プロジェクトA",
+		quantity: 100,
+		unitPrice: 1000,
+	},
+	{
+		name: "プロジェクトB",
+		quantity: 10, // 作業時間
+		unitPrice: 100000, // 単価
+	},
+] as const satisfies {
+	name: string; // 請求内容
+	quantity: number; // 作業時間
+	unitPrice: number; // 単価（円）
+}[];
+
+// 請求先会社情報
+export const billingCompany = {
+	name: "株式会社さんぷる",
+	address: "東京都千代田区さんぷる町1-2-3",
+} as const satisfies {
+	name: string; // 会社名
+	address: string; // 会社住所
+};
+
+// 振込先銀行情報
+export const billingSourceBank = {
+	bankName: "まるまる銀行",
+	branchName: "ばつばつ支店",
+	accountNumber: "9999999",
+	accountHolder: "サンプルタロウ",
+} as const satisfies {
+	bankName: string; // 銀行名
+	branchName: string; // 支店名
+	accountNumber: string; // 口座番号
+	accountHolder: string; // 口座名義
+};
+
+// 請求元情報
+export const billingSource = [
+	{
+		name: "氏名",
+		value: "サンプル太郎",
+	},
+	{
+		name: "住所",
+		value: ["東京都千代田区永田町１丁目１０−１"],
+	},
+	{
+		name: "電話番号",
+		value: "xxx-xxxx-xxxx",
+	},
+] as const satisfies {
+	name: string; // 項目名
+	value: string | string[]; // 値
+}[];
