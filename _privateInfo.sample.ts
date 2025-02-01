381a903e1740f9e9ca78b44025319825780857b4
rmochi <rm_mugen@yahoo.co.jp>
2025-02-01 14:16:49 +0900

    fix

--- _privateInfo.sample.ts
+++ _privateInfo.sample.ts
@@ -1,5 +1,5 @@
 // 請求内容
-export const billingItems = [
+export const billingItems: BillingItem[] = [
 	{
 		name: "プロジェクトA",
 		quantity: 100,
@@ -7,45 +7,119 @@ export const billingItems = [
 	},
 	{
 		name: "プロジェクトB",
-		quantity: 10, // 作業時間
-		unitPrice: 100000, // 単価
+		quantity: 10,
+		unitPrice: 100000,
 	},
-] as const satisfies {
-	name: string; // 請求内容
-	quantity: number; // 作業時間
-	unitPrice: number; // 単価（円）
-}[];
+];
 
 // 請求先会社情報
-export const billingCompany = {
+export const billingCompany: BillingCompany = {
 	name: "株式会社さんぷる",
 	address: "東京都千代田区さんぷる町1-2-3",
-} as const satisfies {
-	name: string; // 会社名
-	address: string; // 会社住所
 };
 
 // 振込先銀行情報
-export const billingSourceBank = {
+export const billingSourceBank: BillingSourceBank = {
 	bankName: "まるまる銀行",
 	branchName: "ばつばつ支店",
 	accountNumber: "9999999",
 	accountHolder: "サンプルタロウ",
-} as const satisfies {
-	bankName: string; // 銀行名
-	branchName: string; // 支店名
-	accountNumber: string; // 口座番号
-	accountHolder: string; // 口座名義
 };
 
 // 請求元情報
-export const billingSource: {
-	name: string; // 氏名
-	address: string | string[]; // 住所
-	phoneNumber?: string; // 電話番号
-	email?: string; // メールアドレス
-} = {
+export const billingSource: BillingSource = {
 	name: "サンプル太郎",
 	address: ["〒xxx-xxxx", "東京都千代田区永田町１丁目１０−１"],
 	phoneNumber: "xxx-xxxx-xxxx",
-} as const;
+};
+
+// Types
+
+/**
+ * @description 請求内容
+ * @property {string} name - 請求内容
+ * @property {number} quantity - 作業時間
+ * @property {number} unitPrice - 単価（円）
+ */
+type BillingItem = {
+	/**
+	 * 請求内容
+	 */
+	name: string;
+	/**
+	 * 作業時間
+	 */
+	quantity: number;
+	/**
+	 * 単価（円）
+	 */
+	unitPrice: number;
+};
+
+/**
+ * @description 請求先会社情報
+ * @property {string} name - 会社名
+ * @property {string} address - 会社住所
+ */
+type BillingCompany = {
+	/**
+	 * 会社名
+	 */
+	name: string;
+	/**
+	 * 会社住所
+	 */
+	address: string;
+};
+
+/**
+ * @description 振込先銀行情報
+ * @property {string} bankName - 銀行名
+ * @property {string} branchName - 支店名
+ * @property {string} accountNumber - 口座番号
+ * @property {string} accountHolder - 口座名義
+ */
+type BillingSourceBank = {
+	/**
+	 * 銀行名
+	 */
+	bankName: string;
+	/**
+	 * 支店名
+	 */
+	branchName: string;
+	/**
+	 * 口座番号
+	 */
+	accountNumber: string;
+	/**
+	 * 口座名義
+	 */
+	accountHolder: string;
+};
+
+/**
+ * @description 請求元情報
+ * @property {string} name - 氏名
+ * @property {string | string[]} address - 住所
+ * @property {string} phoneNumber - 電話番号
+ * @property {string} email - メールアドレス
+ */
+type BillingSource = {
+	/**
+	 * 氏名 or 会社名
+	 */
+	name: string;
+	/**
+	 * 住所
+	 */
+	address: string | string[];
+	/**
+	 * 電話番号
+	 */
+	phoneNumber?: string;
+	/**
+	 * メールアドレス
+	 */
+	email?: string;
+};
